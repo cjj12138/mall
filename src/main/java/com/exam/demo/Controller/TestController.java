65105dc43249669579e3bf7ede8221d3396601db
chen <1215716506@qq.com>
2020-03-21 19:21:37 +0800

    初始框架的搭建

--- src/main/java/com/exam/demo/Controller/TestController.java
+++ src/main/java/com/exam/demo/Controller/TestController.java
@@ -3,17 +3,19 @@ package com.exam.demo.Controller;
 import com.exam.demo.Service.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class TestController {
     @Autowired
     TestService testService;
 
-    @RequestMapping(value = "/test")
-    public String Test(){
-        System.out.println(this.testService.check().getUsername());
-        return "hello,world!";
+    @RequestMapping(value={"/index","/","/hello"})
+    public String index(Model model){
+        model.addAttribute("title", "测试");
+        model.addAttribute("atext", "这个冬天不太Cool");
+        return "index";
     }
 }
